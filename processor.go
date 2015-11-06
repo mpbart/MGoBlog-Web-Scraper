f39798843db63adf581e8232a364557048b70544
Michael Barton <michael.barton@spottradingllc.com>
2015-11-06 13:30:58 -0600

    Formatted code with go fmt

--- processor.go
+++ processor.go
@@ -1,111 +1,111 @@
 package main
 
-import ( 
+import (
+	"fmt"
+	"sort"
 	"strings"
-	 "sort"
-	 "fmt"
-	 )
+)
 
-type WordCounter map[string]int 
+type WordCounter map[string]int
 type WordFrequency struct {
-    word string
-    frequency int
+	word      string
+	frequency int
 }
 type SortableFrequency []WordFrequency
 
 var finalResults = WordCounter{}
 
 func isSpecialCharacter(word string) bool {
-    if len(word) == 0 || isNumber(rune(word[0])) || isPunctuation(rune(word[0])) {
-	return true
-    }
-    return false
+	if len(word) == 0 || isNumber(rune(word[0])) || isPunctuation(rune(word[0])) {
+		return true
+	}
+	return false
 }
 
 func isNumber(c rune) bool {
-    return strings.ContainsRune("1234567890", c)
+	return strings.ContainsRune("1234567890", c)
 }
 
 func isPunctuation(c rune) bool {
-    return strings.ContainsRune("!@#$%^&*?.|~,/\\=+", c)
+	return strings.ContainsRune("!@#$%^&*?.|~,/\\=+", c)
 }
 
 func CountWords(article Article) WordCounter {
-    articleCounter := WordCounter{}
-    for _,str := range article.Content {
-	words := strings.Split(str, " ")
-	for _,word := range words {
-	    if !isSpecialCharacter(word) {
-		baseWord := removeExtras(word)
-		if _,exists := articleCounter[baseWord]; exists == false {
-		    articleCounter[baseWord] = 1
-		} else {
-		    articleCounter[baseWord] += 1
+	articleCounter := WordCounter{}
+	for _, str := range article.Content {
+		words := strings.Split(str, " ")
+		for _, word := range words {
+			if !isSpecialCharacter(word) {
+				baseWord := removeExtras(word)
+				if _, exists := articleCounter[baseWord]; exists == false {
+					articleCounter[baseWord] = 1
+				} else {
+					articleCounter[baseWord] += 1
+				}
+			}
 		}
-	    }
 	}
-    }
-    return articleCounter
+	return articleCounter
 }
 
 func removeExtras(word string) string {
-    word = strings.ToLower(word)
-    if len(word) > 2 && word[len(word)-2:] == "'s" {
-	word = word[:len(word)-2]
-    }
-    if strings.ContainsRune("'\"([{", rune(word[0])) {
-	word = word[1:]
-    }
-    for {
-	if len(word) == 0 {
-	    break
+	word = strings.ToLower(word)
+	if len(word) > 2 && word[len(word)-2:] == "'s" {
+		word = word[:len(word)-2]
 	}
-	if strings.ContainsRune("\"?.,!)]}*':;", rune(word[len(word)-1])) {
-	    word = word[:len(word)-1]
-	} else {
-	    break
+	if strings.ContainsRune("'\"([{", rune(word[0])) {
+		word = word[1:]
 	}
-    }
-    return word
+	for {
+		if len(word) == 0 {
+			break
+		}
+		if strings.ContainsRune("\"?.,!)]}*':;", rune(word[len(word)-1])) {
+			word = word[:len(word)-1]
+		} else {
+			break
+		}
+	}
+	return word
 }
 
 func (arr SortableFrequency) Len() int {
-    return len(arr)
+	return len(arr)
 }
 
 func (arr SortableFrequency) Less(i, j int) bool {
-    if arr[i].frequency > arr[j].frequency {
-	return true
-    }
-    return false
+	if arr[i].frequency > arr[j].frequency {
+		return true
+	}
+	return false
 }
 
 func (arr SortableFrequency) Swap(i, j int) {
-    a, b := arr[i], arr[j]
-    arr[i], arr[j] = b, a
+	a, b := arr[i], arr[j]
+	arr[i], arr[j] = b, a
 }
 
 func aggregateResults(words WordCounter) {
-    for word,count := range words {
-	if _,exists := finalResults[word]; exists == false {
-	    finalResults[word] = count
-	} else {
-	    finalResults[word] += count
+	for word, count := range words {
+		if _, exists := finalResults[word]; exists == false {
+			finalResults[word] = count
+		} else {
+			finalResults[word] += count
+		}
 	}
-    }
 }
 func printTopNResults(numberToPrint int) {
-    frequencies := make(SortableFrequency, len(finalResults))
-    idx := 0
-    for key, value := range finalResults {
-	if ExcludeWordSet.Contains(key) {
-	    continue
+	frequencies := make(SortableFrequency, len(finalResults))
+	idx := 0
+	for key, value := range finalResults {
+		if ExcludeWordSet.Contains(key) {
+			continue
+		}
+		frequencies[idx] = WordFrequency{word: key, frequency: value}
+		idx += 1
 	}
-	frequencies[idx] = WordFrequency{word: key, frequency: value} 
-	idx += 1
-    }
-    sort.Sort(frequencies)
-    for _,i := range frequencies[:numberToPrint] {
-	fmt.Println(i)
-    }
+	sort.Sort(frequencies)
+	for _, i := range frequencies[:numberToPrint] {
+		fmt.Println(i)
+	}
 }
--- scraper.go
+++ scraper.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
 	"flag"
-	"net/http"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"net/http"
 	"os"
-	"bytes"
-        "time"
-	"sync"
 	"strconv"
-       )
+	"sync"
+	"time"
+)
 
 var parseUntil string
 var shouldScrape bool
@@ -23,110 +23,110 @@ var nextPage = "?page="
 var page = 0
 
 func init() {
-    flag.StringVar(&parseUntil, "date", "2015-07-01", "Date to parse until in the format of <YYYY-MM-DD>")
-    flag.BoolVar(&shouldScrape, "scrape", false, "Flag indicating whether to scrape mgoblog")
-    flag.BoolVar(&shouldProcess, "process", false, "Flag indicating whether to process scraped articles")
-    flag.Parse()
-    parseUntilDate, _ = time.Parse("2006-01-02", parseUntil) 
+	flag.StringVar(&parseUntil, "date", "2015-07-01", "Date to parse until in the format of <YYYY-MM-DD>")
+	flag.BoolVar(&shouldScrape, "scrape", false, "Flag indicating whether to scrape mgoblog")
+	flag.BoolVar(&shouldProcess, "process", false, "Flag indicating whether to process scraped articles")
+	flag.Parse()
+	parseUntilDate, _ = time.Parse("2006-01-02", parseUntil)
 }
 
 type RawArticle struct {
-    article Article
-    reader io.Reader
+	article Article
+	reader  io.Reader
 }
 
 func (m *RawArticle) Work() WordCounter {
-    m.article,_ = ProcessArticle(m.reader)
-    for _,tag := range m.article.metadata.Tags {
-	if ExcludeTagSet.Contains(tag) {
-	    return WordCounter{}
+	m.article, _ = ProcessArticle(m.reader)
+	for _, tag := range m.article.metadata.Tags {
+		if ExcludeTagSet.Contains(tag) {
+			return WordCounter{}
+		}
 	}
-    }
-    retval := CountWords(m.article) 
-    return retval
+	retval := CountWords(m.article)
+	return retval
 }
 
 func main() {
-    // Main entry point
-    if shouldScrape {
-	for {
-	    webLink := baseLink + nextPage + strconv.Itoa(page)
-	    _, response, _ := requestPage(webLink)
-	    page += 1
-	    metadata, _ := parseMetadataForArticles(response)
-	    for _, article := range metadata {
-		if !fileExists(directory + article.Title)  && parseUntilDate.Before(article.Date) {
-		    articleContents, _, err := requestPage(article.link)
-		    if err != nil {
-			fmt.Println("Error requesting page ", article.link, err)
-			continue
-		    } 
-		    fmt.Println("Requesting/caching article: ", article.Title)
-		    cachePage(article.Title, articleContents)
-		} else {
-		    break
+	// Main entry point
+	if shouldScrape {
+		for {
+			webLink := baseLink + nextPage + strconv.Itoa(page)
+			_, response, _ := requestPage(webLink)
+			page += 1
+			metadata, _ := parseMetadataForArticles(response)
+			for _, article := range metadata {
+				if !fileExists(directory+article.Title) && parseUntilDate.Before(article.Date) {
+					articleContents, _, err := requestPage(article.link)
+					if err != nil {
+						fmt.Println("Error requesting page ", article.link, err)
+						continue
+					}
+					fmt.Println("Requesting/caching article: ", article.Title)
+					cachePage(article.Title, articleContents)
+				} else {
+					break
+				}
+			}
 		}
-	    }
 	}
-    }
-    if shouldProcess {
-	fmt.Println("Processing...")
-	processCachedArticles()
-    }
+	if shouldProcess {
+		fmt.Println("Processing...")
+		processCachedArticles()
+	}
 }
 
 func processCachedArticles() {
-    myChan := make(chan WordCounter)
+	myChan := make(chan WordCounter)
 
-    go func() {
-	for result := range myChan {
-	    aggregateResults(result)
+	go func() {
+		for result := range myChan {
+			aggregateResults(result)
+		}
+	}()
+
+	files, _ := ioutil.ReadDir("mgoblog.com")
+	var done sync.WaitGroup
+	for _, fileInfo := range files {
+		page := retrieveCachedPage("mgoblog.com/" + fileInfo.Name())
+		done.Add(1)
+		go func(page io.Reader) {
+			newthing := RawArticle{reader: page}
+			results := newthing.Work()
+			myChan <- results
+			done.Done()
+		}(page)
 	}
-    }()
-    
-    files,_ := ioutil.ReadDir("mgoblog.com")
-    var done sync.WaitGroup
-    for _, fileInfo := range files {
-	page := retrieveCachedPage("mgoblog.com/" + fileInfo.Name())
-	done.Add(1)
-	go func(page io.Reader) {
-	    newthing := RawArticle{reader: page}
-	    results := newthing.Work()
-	    myChan <-results
-	    done.Done()
-	}(page)
-    }
-    done.Wait()
-    printTopNResults(50)
+	done.Wait()
+	printTopNResults(50)
 }
 
 func retrieveCachedPage(title string) io.Reader {
-    content,err := ioutil.ReadFile(title)
-    if err != nil {
-	fmt.Println("Error reading file", title, err)
-    }
-    return bytes.NewReader(content)
+	content, err := ioutil.ReadFile(title)
+	if err != nil {
+		fmt.Println("Error reading file", title, err)
+	}
+	return bytes.NewReader(content)
 }
 
 func cachePage(title, content string) {
-    title = SanitizeFilename(title)
-    f,_ := os.Create(directory + title)
-    defer f.Close()
-    f.WriteString(content)
-    f.Sync()
+	title = SanitizeFilename(title)
+	f, _ := os.Create(directory + title)
+	defer f.Close()
+	f.WriteString(content)
+	f.Sync()
 }
 
 func requestPage(link string) (string, io.Reader, error) {
-    
-    resp, err := http.Get(link)
-    if err != nil {
-	fmt.Printf("ERROR: failure to get link %s\n", link)
-	return "", nil, err
-    }
-    contents, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-	fmt.Printf("ERROR: failure to read response body: %s\n", err)
-	return "", nil, err
-    }
-    return string(contents), bytes.NewReader(contents), nil
+
+	resp, err := http.Get(link)
+	if err != nil {
+		fmt.Printf("ERROR: failure to get link %s\n", link)
+		return "", nil, err
+	}
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("ERROR: failure to read response body: %s\n", err)
+		return "", nil, err
+	}
+	return string(contents), bytes.NewReader(contents), nil
 }
